5ff3c1ea0846fdc991ff9b484ad447cbfbc1beb2
hubo88 <hubo88@jd.com>
2022-05-28 23:21:21 +0800

    add test

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author hubo88
+ * @description
+ * @date 2022/5/12 12:51 PM
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
